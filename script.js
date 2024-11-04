be8917a5da85696d3871f70b0a91e52a0aa3f27d
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-11-05 01:08:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function() {
+
+  var flame = $('#flame');
+  var txt = $('h1');
+
+  flame.on({
+    click: function() {
+      flame.removeClass('burn').addClass('puff');
+      $('.smoke').each(function() {
+        $(this).addClass('puff-bubble');
+      });
+      $('#glow').remove();
+      txt.hide().html("It'll come true! I believe in you.").delay(750).fadeIn(300);
+      $('#candle').animate({
+        'opacity': '.5'
+      }, 100);
+    }
+  })
+});
